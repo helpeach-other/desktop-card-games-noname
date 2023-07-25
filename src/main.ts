4a7bce657ec898c0ca22f96c3f71cc26bcd69cc4
siykt <cnsiykt@163.com>
2023-07-26 00:48:45 +0800

    feat: reset style

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { setupRouter } from './router'
 import { setupStore } from './store'
 import './samples/node-api'
+import './styles/global.css'
 
 async function bootstrap() {
   const app = createApp(App)
